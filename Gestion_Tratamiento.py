712799943afd7867e13357b8ff50d6fc58449cd2
Evelyn Paloma <eve.paloma92e@mail.com>
2024-10-06 12:14:41 -0300

    Se agrego funcion activar edición y se centraron las ventnas.

--- Gestion_Tratamiento.py
+++ Gestion_Tratamiento.py
@@ -36,11 +36,13 @@ class GestionTratamiento(Frame):
 
         img_buscar = Image.open("buscar1.png").resize((30, 30), Image.Resampling.LANCZOS)
         img_buscar = ImageTk.PhotoImage(img_buscar)
-        btn_buscar = Button(frame_busqueda, image=img_buscar, width=30, height=30,bg="#e6c885", command=self.buscar_tratamiento)
+        btn_buscar = Button(frame_busqueda, image=img_buscar, width=30, height=30,bg="#e6c885", 
+                            command=self.buscar_tratamiento)
         btn_buscar.grid(row=1, column=3)
         btn_buscar.image = img_buscar
 
-        boton_agregar = Button(frame_tratamientos, text="Agregar   +", width=15, bg="#e6c885",font=("Robot",13), command=self.agregar_tratamiento)
+        boton_agregar = Button(frame_tratamientos, text="Agregar   +", width=15, bg="#e6c885",font=("Robot",13),
+                                command=self.agregar_tratamiento)
         boton_agregar.grid(row=1, column=5, padx=10, pady=10)
 
         #Para que siempre esté atrás de los widgets
@@ -85,13 +87,16 @@ class GestionTratamiento(Frame):
         frame_btn.grid(row=4, columnspan=6)
 
         #Botones(ver, modificar, eliminar)
-        btn_ver = Button(frame_btn, text="Ver", width=15, font=("Robot",13),bg="#e6c885", command=self.ver_tratamiento)
+        btn_ver = Button(frame_btn, text="Ver", width=15, font=("Robot",13),bg="#e6c885", 
+                         command=self.ver_tratamiento)
         btn_ver.grid(row=4, column=1,padx=50)
 
-        btn_editar = Button(frame_btn, text="Modificar", width=15, font=("Robot",13),bg="#e6c885", command=self.modificar_tratamiento)
+        btn_editar = Button(frame_btn, text="Modificar", width=15, font=("Robot",13),bg="#e6c885",
+                             command=self.modificar_tratamiento)
         btn_editar.grid(row=4, column=2,padx=50)
         
-        btn_eliminar = Button(frame_btn, text="Eliminar", width=15,font=("Robot",13),bg="#e6c885", command=self.eliminar_tratamiento)
+        btn_eliminar = Button(frame_btn, text="Eliminar", width=15,font=("Robot",13),bg="#e6c885",
+                               command=self.eliminar_tratamiento)
         btn_eliminar.grid(row=4, column=3, padx=50)
 
     def agregar_tratamiento(self):
@@ -113,7 +118,8 @@ class GestionTratamiento(Frame):
             entry.grid(row=i, column=1, padx=10, pady=5)
             entradas[campo] = entry
 
-        btn_nuevo_tratamiento = Button(frame_agregar, text="Agregar", font=("Robot", 10),bg="#e6c885", command=lambda: self.guardar_nuevo_tratamiento(entradas, ventana_agregar))
+        btn_nuevo_tratamiento = Button(frame_agregar, text="Agregar", font=("Robot", 10),bg="#e6c885", 
+                                       command=lambda: self.guardar_nuevo_tratamiento(entradas, ventana_agregar))
         btn_nuevo_tratamiento.grid(row=len(campos), column=0, columnspan=2, padx=10, pady=10)
 
     def ver_tratamiento(self):
@@ -139,6 +145,7 @@ class GestionTratamiento(Frame):
         ventana.title("Detalles del Tratamiento")
         ventana.config(bg="#e4c09f")
         ventana.resizable(False,False)
+        ventana.geometry("510x345+400+160")
         
         ventana.grid_columnconfigure(0, weight=1)
         ventana.grid_rowconfigure(0, weight=1)
@@ -161,17 +168,20 @@ class GestionTratamiento(Frame):
 
             if modo == "ver":
                 entry.config(state="readonly")
-                btn_editar = Button(ventana, text="Modificar", width=15, font=("Robot", 13), bg="#e6c885",command=lambda: self.activar_edicion(entradas, btn_guardar))
+                btn_editar = Button(ventana, text="Modificar", width=15, font=("Robot", 13), bg="#e6c885",
+                                    command=lambda: self.activar_edicion(entradas, btn_guardar))
                 btn_editar.grid(row=len(campos), column=0, pady=10)
 
-    # Botón "Guardar Cambios"
-                btn_guardar = Button(frame_detalles, text="Guardar Cambios", command=lambda: self.guardar_cambios(entradas, ventana, id_seleccionado))
+    
+                btn_guardar = Button(frame_detalles, text="Guardar Cambios", 
+                                     command=lambda: self.guardar_cambios(entradas, ventana))
                 btn_guardar.grid(row=len(campos), column=0, columnspan=2, padx=10, pady=10)
                 btn_guardar.config(state="disabled")  # Iniciar como deshabilitado
                                 
 
         if modo == "modificar":
-            btn_modificar = Button(frame_detalles, text="Guardar Cambios", command=lambda: self.guardar_cambios(entradas, ventana, id_seleccionado))
+            btn_modificar = Button(frame_detalles, text="Guardar Cambios", 
+                                   command=lambda: self.guardar_cambios(entradas, ventana, id_seleccionado))
             btn_modificar.grid(row=len(campos), column=0, columnspan=2, padx=10, pady=10)
 
     def activar_edicion(self, entradas, btn_guardar):
@@ -210,7 +220,7 @@ class GestionTratamiento(Frame):
     def agregar_tratamiento(self):
         ventana_agregar = Toplevel(self)
         ventana_agregar.title("Agregar Tratamiento")
-        ventana_agregar.config(bg="#e4c09f") 
+        ventana_agregar.config(bg="#e4c09f")
         ventana.resizable(False,False)
         
         frame_agregar = LabelFrame(ventana_agregar, text="Agregar Nuevo Tratamiento", font= ("Robot", 11),padx=10, pady=10, bg="#c9c2b2")
@@ -245,9 +255,13 @@ class GestionTratamiento(Frame):
             messagebox.showwarning("Atención", "Complete todos los campos.")
 
     def buscar_tratamiento(self):
-        busqueda = self.entrada_buscar.get().lower()
+        busqueda = self.entrada_buscar.get().strip().lower()
         tratamiento_encontrado = False
-          #Obtenemos búsqueda
+    
+        if not busqueda:
+            self.tree.delete(*self.tree.get_children())
+            self.cargar_tratamiento()
+        #Obtenemos búsqueda
         for item in self.tree.get_children():         #Recorre cada fila usando identificador en la lista devuelta por children
             valores = self.tree.item(item, 'values')  #Obtiene los valores de las columnas de la fila correspondiente al identificador item.
             codigo = valores[0].lower()
@@ -258,12 +272,21 @@ class GestionTratamiento(Frame):
                 tratamiento_encontrado = True
             else:
                 self.tree.detach(item)                #Oculta los otros tratamientos.
-
+       
+            
         if not tratamiento_encontrado:
+            #self.tree.delete(*self.tree.get_children())
+            #self.cargar_tratamiento()
             messagebox.showwarning("Atención", "No se encontró el tratamiento.")
+
+    def cargar_tratamiento(self):
+        self.tree.insert("", "end", values=("1234", "Tratamiento 1", "$100"))
+        self.tree.insert("", "end", values=("5678", "Tratamiento 2", "$150"))
+        self.tree.insert("", "end", values=("91011", "Tratamiento 3", "$200"))
 
 ventana = Tk()
 ventana.title("Gestion de Tratamientos")
 ventana.resizable(False,False)
+ventana.geometry("+200+80")
 root = GestionTratamiento(ventana)
 ventana.mainloop()
