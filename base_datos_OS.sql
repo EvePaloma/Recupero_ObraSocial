55dba822490aa38a19ae775729efda67fc5cd2bc
Catherine Bustos Vazquez <123335198+Catyvaz@users.noreply.github.com>
2024-10-14 22:44:44 -0300

    simulacro de base de datos

---
+++ base_datos_OS.sql
@@ -0,0 +1,167 @@
+DROP DATABASE IF EXISTS `obra_social`;
+CREATE DATABASE `obra_social`;
+USE obra_social;
+DROP TABLE IF EXISTS `tipo_documento`;
+CREATE TABLE `tipo_documento` (
+  `id_tipo_documento` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_tipo_documento`)
+);
+DROP TABLE IF EXISTS `rol`;
+CREATE TABLE `rol` (
+  `id_rol` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_rol`)
+);
+DROP TABLE IF EXISTS `afip`;
+CREATE TABLE `afip` (
+  `id_afip` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_afip`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`)
+);
+DROP TABLE IF EXISTS `pais`;
+CREATE TABLE `pais` (
+  `id_pais` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_pais`)
+);
+DROP TABLE IF EXISTS `ciudad`;
+CREATE TABLE `ciudad` (
+  `id_ciudad` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `id_pais` int NOT NULL,
+  PRIMARY KEY (`id_ciudad`),
+  FOREIGN KEY (`id_pais`) REFERENCES `pais` (`id_pais`)
+);
+DROP TABLE IF EXISTS `barrio`;
+CREATE TABLE `barrio` (
+  `id_barrio` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `id_ciudad` int NOT NULL,
+  PRIMARY KEY (`id_barrio`),
+  FOREIGN KEY (`id_ciudad`) REFERENCES `ciudad` (`id_ciudad`)
+);
+
+DROP TABLE IF EXISTS `especialidad`;
+CREATE TABLE `especialidad` (
+  `id_especialidad` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `activo` tinyint NOT NULL DEFAULT TRUE,
+  PRIMARY KEY (`id_especialidad`)
+);
+DROP TABLE IF EXISTS `medico`;
+CREATE TABLE `medico` (
+  `id_medico` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `matricula` varchar(45) NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `tipo_documento` int NOT NULL DEFAULT '1',
+  `documento` varchar(45) NOT NULL DEFAULT '1',
+  `activo` tinyint NOT NULL DEFAULT 1,
+  `id_especialidad` int NOT NULL,
+  PRIMARY KEY (`id_medico`),
+  UNIQUE KEY `documento_UNIQUE` (`documento`),
+  FOREIGN KEY (`id_especialidad`) REFERENCES `especialidad` (`id_especialidad`),
+  FOREIGN KEY (`tipo_documento`) REFERENCES `tipo_documento` (`id_tipo_documento`)
+);
+
+DROP TABLE IF EXISTS `obra_social`;
+CREATE TABLE `obra_social` (
+  `id_obra_social` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `siglas` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `anotacion` longtext,
+  `domicilio_central` varchar(45) NOT NULL,
+  `domicilio_cba` varchar(45) DEFAULT NULL,
+  `cuit` varchar(45) NOT NULL,
+  `id_afip` int DEFAULT NULL,
+  `activo` tinyint NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id_obra_social`),
+  UNIQUE KEY `id_obra_social_UNIQUE` (`id_obra_social`),
+  FOREIGN KEY (`id_afip`) REFERENCES `afip` (`id_afip`)
+);
+
+DROP TABLE IF EXISTS `paciente`;
+CREATE TABLE `paciente` (
+  `id_paciente` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `nacimiento` date NOT NULL,
+  `direccion` varchar(60) NOT NULL,
+  `id_barrio` int NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `telefono_familiar` varchar(45) DEFAULT NULL,
+  `tipo_documento` int NOT NULL,
+  `documento` varchar(45) NOT NULL,
+  `activo` tinyint NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id_paciente`),
+  FOREIGN KEY (`id_barrio`) REFERENCES `barrio` (`id_barrio`),
+  FOREIGN KEY (`tipo_documento`) REFERENCES `tipo_documento` (`id_tipo_documento`)
+);
+DROP TABLE IF EXISTS `detalle_obra_social`;
+CREATE TABLE `detalle_obra_social` (
+  `id_detalle_os` int NOT NULL AUTO_INCREMENT,
+  `id_paciente` int NOT NULL,
+  `id_obra_social` int NOT NULL,
+  `nro_afiliado` varchar(25) NOT NULL,
+  `caracter` varchar(25) NOT NULL,
+  `activo` tinyint NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id_detalle_os`),
+  FOREIGN KEY (`id_paciente`) REFERENCES `paciente` (`id_paciente`),
+  FOREIGN KEY (`id_obra_social`) REFERENCES `obra_social` (`id_obra_social`)
+);
+
+DROP TABLE IF EXISTS `tratamiento`;
+CREATE TABLE `tratamiento` (
+  `id_tratamiento` int NOT NULL AUTO_INCREMENT,
+  `cod_tratamiento` varchar(45) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `descripcion` varchar(50),
+  `precio` float NOT NULL,
+  `fecha_precio` date NOT NULL,
+  `activo` tinyint NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id_tratamiento`)
+);
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE `usuario` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `legajo` varchar(45) NOT NULL,
+  `clave` varchar(45) NOT NULL,
+  `activo` tinyint NOT NULL DEFAULT 1,
+  `id_rol` int NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`)
+);
+
+DROP TABLE IF EXISTS `ficha`;
+CREATE TABLE `ficha` (
+  `id_ficha` int NOT NULL AUTO_INCREMENT,
+  `id_paciente` int NOT NULL,
+  `id_obra_social` int NOT NULL,
+  `id_medico` int NOT NULL,
+  `fecha` date NOT NULL,
+  `activo` tinyint NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id_ficha`),
+  FOREIGN KEY (`id_paciente`) REFERENCES `paciente` (`id_paciente`),
+  FOREIGN KEY (`id_obra_social`) REFERENCES `obra_social` (`id_obra_social`),
+  FOREIGN KEY (`id_medico`) REFERENCES `medico` (`id_medico`)
+);
+
+DROP TABLE IF EXISTS `detalle_ficha`;
+CREATE TABLE `detalle_ficha` (
+  `id_detalle` int NOT NULL AUTO_INCREMENT,
+  `id_ficha` int NOT NULL,
+  `id_tratamiento` int NOT NULL,
+  `cantidad` int NOT NULL,
+  `precio_unitario` float NOT NULL,
+  `subtotal` float AS (cantidad * precio_unitario) STORED,
+  PRIMARY KEY (`id_detalle`),
+  FOREIGN KEY (`id_ficha`) REFERENCES `ficha` (`id_ficha`),
+  FOREIGN KEY (`id_tratamiento`) REFERENCES `tratamiento` (`id_tratamiento`)
+);
