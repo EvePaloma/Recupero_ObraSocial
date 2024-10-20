0dcd35cafe60eac8d8e27b8c78dcdaccd987016f
Evelyn Paloma <eve.paloma92e@mail.com>
2024-10-20 17:54:31 -0300

    Se modificó todo el código para interactuar con BD por id

--- Gestion_Tratamiento.py
+++ Gestion_Tratamiento.py
@@ -55,7 +55,7 @@ class GestionTratamiento(Frame):
         btn_buscar.image = img_buscar
 
         boton_agregar = Button(frame_tratamientos, text="Agregar   +", width=15, bg="#e6c885",font=("Robot",13),
-                                command=self.buscar_tratamiento)
+                                command=self.agregar_tratamiento)
         boton_agregar.grid(row=1, column=5, padx=10, pady=10)
 
         #Para que siempre esté atrás de los widgets
@@ -114,106 +114,122 @@ class GestionTratamiento(Frame):
         ventana_agregar = Toplevel(self)
         ventana_agregar.title("Agregar Tratamiento")
         ventana_agregar.config(bg="#e4c09f") 
-        ventana.resizable(False,False)
+        ventana_agregar.resizable(False,False)
+        ventana_agregar.geometry("455x310+400+160")
 
         frame_agregar = LabelFrame(ventana_agregar, text="Agregar Nuevo Tratamiento", font= ("Robot", 12),padx=10, pady=10, bg="#c9c2b2")
         frame_agregar.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
-        campos = ["Código", "Nombre", "Precio", "Fecha Precio", "Tipo", "Siglas", "Descripción"]
+        campos = ["Código", "Nombre", "Precio", "Fecha Precio", "Siglas", "Descripción"]
         entradas = {}
 
         for i, campo in enumerate(campos):     #Devuelve índice y valor de cada elemento 
             etiquetas = Label(frame_agregar, text=campo + ":", bg="#c9c2b2", font=("Robot", 10))
             etiquetas.grid(row=i, column=0, padx=10, pady=5)
-            if campo == "Tipo":
-                entry_tipo = ttk.Combobox(frame_agregar, width=37, font=("Robot", 10), state="readonly")
-                entry_tipo['values'] = ("Consulta", "Cirugía", "Terapia", "Examen", "Otro")
-                entry_tipo.grid(row=i, column=1, padx=10, pady=5)
-                entry_tipo.set("Consulta")
-            else:
-                entry = Entry(frame_agregar, width=40, font=("Robot", 10))
-                entry.grid(row=i, column=1, padx=10, pady=5)
+            entry = Entry(frame_agregar, width=40, font=("Robot", 10))
+            entry.grid(row=i, column=1, padx=10, pady=5)
             entradas[campo] = entry
 
         btn_nuevo_tratamiento = Button(frame_agregar, text="Agregar", font=("Robot", 10),bg="#e6c885", 
                                        command=lambda: self.guardar_nuevo_tratamiento(entradas, ventana_agregar))
         btn_nuevo_tratamiento.grid(row=len(campos), column=0, columnspan=2, padx=10, pady=10)
 
+    
+
     def ver_tratamiento(self):
         seleccion = self.tree.selection()
         if not seleccion:
             messagebox.showwarning("Atención", "Por favor, seleccione un tratamiento.")
             return
+
+        # Usamos el primer elemento seleccionado (ID oculto)
+        id_seleccionado = seleccion[0]
         
-        tratamiento_seleccionado = self.tree.item(seleccion[0], 'values')   #Item= valor del elemento
-        self.abrir_ventana_tratamiento(tratamiento_seleccionado,seleccion[0],modo="ver")
+        # Obtenemos el tratamiento usando el ID
+        tratamiento_seleccionado = self.obtener_tratamiento_por_id(id_seleccionado)
+
+        if tratamiento_seleccionado:
+            # Abrimos la ventana sin mostrar el ID
+            tratamiento_reducido = tratamiento_seleccionado[0:]  # Aquí excluimos el ID
+            self.abrir_ventana_tratamiento(tratamiento_reducido, modo="ver",seleccion=id_seleccionado)  # Excluimos el ID
+        else:
+            messagebox.showerror("Error", "No se pudo obtener el tratamiento.")
 
     def modificar_tratamiento(self):
         seleccion = self.tree.selection()
         if not seleccion:
             messagebox.showwarning("Atención", "Por favor, seleccione un tratamiento.")
             return
+        # Usamos el primer elemento seleccionado (ID oculto)
+        id_seleccionado = seleccion[0]
+        # Aquí obtenemos los valores del tratamiento usando el ID
+        tratamiento_seleccionado = self.obtener_tratamiento_por_id(id_seleccionado)
+        if tratamiento_seleccionado:
+            # Abrimos la ventana sin mostrar el ID
+            tratamiento_reducido = tratamiento_seleccionado[0:]  # Aquí excluimos el ID
+            self.abrir_ventana_tratamiento(tratamiento_reducido, modo="modificar", seleccion=id_seleccionado)
+        else:
+            messagebox.showerror("Error", "No se pudo obtener el tratamiento para modificar.")
         
-        tratamiento_seleccionado = self.tree.item(seleccion[0], 'values')
-        self.abrir_ventana_tratamiento(tratamiento_seleccionado, seleccion[0],modo="modificar")    
-    
-    def abrir_ventana_tratamiento(self, tratamiento, id_seleccionado, modo="ver"):
+    def abrir_ventana_tratamiento(self, tratamiento,modo, seleccion=None):           
         ventana = Toplevel(self)
         ventana.title("Detalles del Tratamiento")
         ventana.config(bg="#e4c09f")
-        ventana.resizable(False,False)
+        ventana.resizable(False, False)
         ventana.geometry("510x345+400+160")
         
-        ventana.grid_columnconfigure(0, weight=1)
-        ventana.grid_rowconfigure(0, weight=1)
+        ventana.grid_columnconfigure(0, weight=2)
+        ventana.grid_rowconfigure(0, weight=2)
 
         frame_detalles = LabelFrame(ventana, text="Detalles del Tratamiento", font=("Robot", 10), padx=10, pady=10, bg="#c9c2b2")
         frame_detalles.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
-        campos = ["Código", "Nombre", "Precio", "Fecha Precio", "Tipo", "Siglas", "Descripción"]
-        valores = list(tratamiento) + ["Tipo Ejemplo", "Ejemplo Fecha Precio","Siglas Ejemplo", "Descripción"]  #ejemplo
-        entradas ={}
+        campos = ["Código", "Nombre", "Precio", "Fecha Precio", "Siglas", "Descripción"]
+        valores = list(tratamiento)
+        entradas = {}
 
         for i, campo in enumerate(campos):
             etiqueta = Label(frame_detalles, text=campo + ":", bg="#c9c2b2", font=("Robot", 10))
             etiqueta.grid(row=i, column=0, padx=10, pady=5)
-            if campo == "Tipo":
-                entry = ttk.Combobox(frame_detalles, width=37, font=("Robot", 10), state="readonly")
-                entry['values'] = ("Consulta", "Cirugía", "Terapia", "Examen", "Otro")
-                entry.grid(row=i, column=1, padx=10, pady=5)
-                entry.set("Consulta")
-                if modo == "ver":
-                  entry.config(state="disabled")
-            else:
-                entry = Entry(frame_detalles, width=40)
-                entry.grid(row=i, column=1, padx=10, pady=5)
-                entry.insert(0, valores[i])
-            entradas[campo] = entry
-            
+            entry = Entry(frame_detalles, width=40, font=("Robot", 10))
+            entry.grid(row=i, column=1, padx=10, pady=5)
+            if i + 1 < len(valores):
+                entry.insert(0,str(valores[i + 1])) 
+                entradas[campo] = entry
 
-            if modo == "ver":
-                entry.config(state="readonly")  
+        if modo == "ver":
+                for entry in entradas.values():
+                    entry.config(state="readonly")
                 
-                btn_editar = Button(ventana, text="Modificar", width=15, font=("Robot", 13), bg="#e6c885",
-                                    command=lambda: self.activar_edicion(entradas, btn_guardar))
-                btn_editar.grid(row=len(campos), column=0, pady=10)
+                frame_btns = Frame(ventana, bg="#e4c09f")
+                frame_btns.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
 
-    
-                btn_guardar = Button(frame_detalles, text="Guardar Cambios", 
-                                     command=lambda: self.guardar_cambios(entradas, ventana))
-                btn_guardar.grid(row=len(campos), column=0, columnspan=2, padx=10, pady=10)
+                btn_editar = Button(frame_btns, text="Modificar", width=15, font=("Robot", 13), bg="#e6c885",
+                                    command=lambda: self.activar_edicion(entradas,btn_guardar))
+                btn_editar.grid(row=len(campos), column=0, padx=40,pady=10)
+
+                btn_guardar = Button(frame_btns, text="Guardar Cambios", width=15, font=("Robot", 13), bg="#e6c885", 
+                                    command=lambda: self.guardar_cambios(entradas, ventana, seleccion))
+                btn_guardar.grid(row=len(campos), column=1, padx=50,pady=10)
                 btn_guardar.config(state="disabled")  # Iniciar como deshabilitado
-                                
 
         if modo == "modificar":
-            btn_modificar = Button(frame_detalles, text="Guardar Cambios", 
-                                   command=lambda: self.guardar_cambios(entradas, ventana, id_seleccionado))
-            btn_modificar.grid(row=len(campos), column=0, columnspan=2, padx=10, pady=10)
+            frame_btns = Frame(ventana, bg="#e4c09f")
+            frame_btns.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+
+            btn_guardar = Button(frame_btns, text="Guardar Cambios", width=15, font=("Robot", 13), bg="#e6c885",
+                                command=lambda: self.guardar_cambios(entradas, ventana, seleccion))
+            btn_guardar.grid(row=len(campos), column=0,  padx=60, pady=10)
+
+            btn_cancelar = Button(frame_btns, text="Cancelar", width=15, font=("Robot", 13), bg="#e6c885",
+                                command=ventana.destroy)
+            btn_cancelar.grid(row=len(campos), column=1, pady=10)
+
 
     def activar_edicion(self, entradas, btn_guardar):
         for entry in entradas.values():
-            entry.config(state="normal")  # Permitir edición en todos los Entry
-        btn_guardar.config(state="normal")  # Activar el botón directamente
+            entry.config(state="normal")  
+        btn_guardar.config(state="normal") 
 
     def guardar_cambios(self, entradas, ventana,seleccion):
         conexion = obtener_conexion()
@@ -221,30 +237,39 @@ class GestionTratamiento(Frame):
             messagebox.showerror("Error", "No se pudo conectar a la base de datos.")
             return
         nuevos_valores = {campo: entradas[campo].get() for campo in entradas}
-        self.tree.item(seleccion, values=list(nuevos_valores.values()))
-        try:
-            cursor = conexion.cursor()
-            sql = "UPDATE tratamiento SET codigo=%s, nombre=%s, precio=%s, fecha_precio=%s, tipo_tratamiento=%s, siglas=%s, descripcion=%s WHERE codigo=%s"
-            val = (nuevos_valores['Código'], 
-                   nuevos_valores['Nombre'],
-                   nuevos_valores['Precio'],
-                   nuevos_valores['Fecha Precio'], 
-                   nuevos_valores['Tipo'], 
-                   nuevos_valores['Siglas'], 
-                   nuevos_valores['Descripción'],
-                   self.tree.item(seleccion, 'values')[0]
-                   )
-            cursor.execute(sql, val)
-            conexion.commit()
-            messagebox.showinfo("Información", "Tratamiento modificado correctamente.")
-            ventana.destroy()
-            self.actualizar_treeview()
-        except mysql.connector.Error as error:
-            messagebox.showerror("Error", f"No se pudo modificar el tratamiento: {error}")
-        finally:
-            cursor.close()
-            conexion.close()
+        print(nuevos_valores)
+        # Asegúrate de que 'seleccion' no sea None y tenga un valor válido
+        if seleccion:
+            try:
+                cursor = conexion.cursor()
+                sql = "UPDATE tratamiento SET codigo=%s, nombre=%s, precio=%s, fecha_precio=%s, siglas=%s, descripcion=%s WHERE id_tratamiento=%s"
+                val = (
+                    nuevos_valores['Código'], 
+                    nuevos_valores['Nombre'],
+                    nuevos_valores['Precio'],
+                    nuevos_valores['Fecha Precio'], 
+                    nuevos_valores['Siglas'], 
+                    nuevos_valores['Descripción'],
+                    seleccion  # Usa el ID original del tratamiento que estás modificando
+                )
 
+                if any(value is None for value in val[:-1]):
+                    messagebox.showerror("Error", "Hay campos vacíos que no pueden ser actualizados.")
+                    return
+                cursor.execute(sql, val)
+                conexion.commit()
+                print(f"Filas afectadas: {cursor.rowcount}")  # Muestra cuántas filas se actualizaron
+                messagebox.showinfo("Información", "Tratamiento modificado correctamente.")
+                ventana.destroy()
+                self.actualizar_treeview()
+                messagebox.showerror("Error", "No se pudo modificar el tratamiento: ")
+                messagebox.showinfo("Éxito", "Tratamiento actualizado correctamente.")
+            except mysql.connector.Error as err:
+                messagebox.showerror("Error", f"Ocurrió un error al actualizar el tratamiento: {err}")
+            finally:
+                cursor.close()
+                conexion.close()
+                
     def eliminar_tratamiento(self):
         conexion = obtener_conexion()
         if conexion is None:
@@ -279,15 +304,14 @@ class GestionTratamiento(Frame):
         nombre = entry["Nombre"].get()
         precio = entry["Precio"].get()
         fecha_precio = entry["Fecha Precio"].get()
-        tipo_tratamiento = entry["Tipo"].get()
         siglas = entry["Siglas"].get()
         descripcion = entry["Descripción"].get()
         # Validar datos y agregar al Treeview
-        if codigo and nombre and precio and fecha_precio and tipo_tratamiento and siglas and descripcion:
+        if codigo and nombre and precio and fecha_precio  and siglas and descripcion:
             try:
                 cursor = conexion.cursor()
-                sql = "INSERT INTO tratamiento (codigo, nombre, precio, fecha_precio, tipo_tratamiento, siglas, descripcion) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                val = (codigo, nombre, precio, fecha_precio, tipo_tratamiento, siglas, descripcion)
+                sql = "INSERT INTO tratamiento (codigo, nombre, precio, fecha_precio, siglas, descripcion) VALUES (%s, %s, %s, %s, %s, %s)"
+                val = (codigo, nombre, precio, fecha_precio, siglas, descripcion)
                 cursor.execute(sql, val)
                 conexion.commit()
                 messagebox.showinfo("Información", "Tratamiento agregado exitosamente")
@@ -302,20 +326,42 @@ class GestionTratamiento(Frame):
         else:
             messagebox.showwarning("Atención", "Complete todos los campos.")
 
+    
     def actualizar_treeview(self):
-        self.tree.delete(*self.tree.get_children())
+        for item in self.tree.get_children():
+            self.tree.delete(item)
         conexion = obtener_conexion()
-        if conexion is None:
-            messagebox.showerror("Error", "No se pudo conectar a la base de datos.")
-            return
+        cursor = conexion.cursor()
+        cursor.execute("SELECT * FROM tratamiento")
+        tratamientos = cursor.fetchall()
+        for tratamiento in tratamientos:
+            self.tree.insert("", "0", iid=tratamiento[0], values=tratamiento[1:])
+        
+        cursor.close()
+        conexion.close()
+    
+    def cargar_tratamientos(self):
+        # Obtener tratamientos de la base de datos
+        tratamientos = self.obtener_tratamientos()  # Asegúrate de que esta función devuelva una lista de tratamientos
+
+        for tratamiento in tratamientos:
+            # Supongamos que el tratamiento es una tupla con (id, codigo, nombre, precio, ...)
+            # Agregamos solo los campos que deseas mostrar
+            self.tree.insert('', 'end', iid=tratamiento[0], values=(tratamiento[1], tratamiento[2], tratamiento[3]))  # Mostrar solo los campos deseados
+
+    def obtener_tratamiento_por_id(self, id_tratamiento):
+        conexion = obtener_conexion()
         try:
+            sql = "SELECT * FROM tratamiento WHERE id_tratamiento = %s"
             cursor = conexion.cursor()
-            cursor.execute("SELECT codigo, nombre, precio FROM tratamiento")
-            tratamientos = cursor.fetchall()
-            for tratamiento in tratamientos:
-                self.tree.insert("", 0, values=tratamiento)
+            cursor.execute(sql, (id_tratamiento,))
+            tratamiento = cursor.fetchone()
+            if tratamiento is None:
+               messagebox.showwarning("Advertencia", "No se encontró ningún tratamiento con ese ID.")
+            
+            return tratamiento
         except mysql.connector.Error as error:
-            messagebox.showerror("Error", f"No se pudo recuperar los tratamientos: {error}")
+            messagebox.showerror("Error", f"No se pudo recuperar el tratamiento: {error}")
         finally:
             cursor.close()
             conexion.close()
