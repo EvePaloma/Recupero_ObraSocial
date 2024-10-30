c3665ad85514894e1052432bbd727c4f60a4a627
Caty <123335198+Catyvaz@users.noreply.github.com>
2024-10-30 00:58:00 -0300

    menu actualizado

--- Menu.py
+++ Menu.py
@@ -8,11 +8,11 @@ from Gestion_Pacientes import *
 
 class MENU(Frame):
     def __init__(self, master):
-        Frame.__init__(self, master, bg="#e4c09f", height=890, width=1366)
+        Frame.__init__(self, master, bg="#e4c09f", height=700, width=1366)
         self.master = master
+        master.geometry("+0+0")
         self.pack_propagate(False)
         self.pack(expand=True)
-        self.grid()
         self.create_widgets()
         self.master.protocol("WM_DELETE_WINDOW", lambda: None)
 
@@ -57,42 +57,48 @@ class MENU(Frame):
         from Login import Login
         
     def create_widgets(self):
-        frame_btn = Frame(self, bg="#e4c09f")
+        ventana_principal = Frame(self, bg="#e4c09f")
+        ventana_principal.pack(fill="both", expand=True, pady= 15)
+
+        frame_btn = Frame(ventana_principal, bg="#e4c09f")
         frame_btn.grid(row=0, column=0, padx=10, pady=(10, 0),sticky="ne")
 
-        btn_cerrar_sesion = Button(frame_btn, text="Cerrar Sesión", font=("Roboto", 13), bg="#c9c2b2", command=self.volver_login)
+        btn_cerrar_sesion = Button(frame_btn, text="Cerrar Sesión", font=("Roboto", 15), width= 15, bg="#c9c2b2", command=self.volver_login)
         btn_cerrar_sesion.grid(row=0, column=0, pady=(0, 10))
 
-        menu = LabelFrame(self, text="Menú", padx=20, pady=20, bg="#c9c2b2", height=530, width=1200)
-        menu.grid(row=0, column=0, padx=80, pady= 80)
+        menu = LabelFrame(ventana_principal, padx=20, pady=20, bg="#c9c2b2", height=530, width=1200)
+        menu.grid(row=0, column=0, pady= 80, padx= 110)
         menu.grid_propagate(False)
 
-        label_menu = LabelFrame(menu, font=("Roboto", 20), bg="#c9c2b2")
-        label_menu.grid(row=1, column=0, columnspan=2, pady=(0, 20))
+
+        label_menu = Frame(menu, bg="#c9c2b2")
+        label_menu.pack(pady= 15)
 
         menu_label = Label(label_menu, text="Menú Principal", font=("Roboto", 20), bg="#c9c2b2", width=67)
         menu_label.grid(row=0, column=0, padx=(0,10), pady=(0, 10))
 
-        btn_paciente = Button(menu, text="Gestión de Paciente", font=("Roboto", 15), bg="#e4c09f", width=96,command=self.abrir_paciente)
+        frame_botones = Frame(menu, bg="#c9c2b2")
+        frame_botones.pack(pady= 15)
+
+        btn_paciente = Button(frame_botones, text="Gestión de Paciente", font=("Roboto", 15), bg="#e4c09f", height=2, width=96,command=self.abrir_paciente)
         btn_paciente.grid(row=2, column=0, pady=(0, 20))
 
-        btn_medico = Button(menu, text="Gestión de Médico", font=("Roboto", 15), bg="#e4c09f", width=96,command=self.abrir_medico)
+        btn_medico = Button(frame_botones, text="Gestión de Médico", font=("Roboto", 15), bg="#e4c09f", height=2, width=96,command=self.abrir_medico)
         btn_medico.grid(row=3, column=0, pady=(0, 20))
 
-        btn_obra_social = Button(menu, text="Gestión de Obra Social", font=("Roboto", 15), bg="#e4c09f", width=96, command=self.abrir_obra_social)
+        btn_obra_social = Button(frame_botones, text="Gestión de Obra Social", font=("Roboto", 15), bg="#e4c09f", height=2, width=96, command=self.abrir_obra_social)
         btn_obra_social.grid(row=4, column=0, pady=(0, 20))
 
-        btn_tratamiento = Button(menu, text="Gestión de Tratamiento", font=("Roboto", 15), bg="#e4c09f", width=96,command=self.abrir_tratamiento)
+        btn_tratamiento = Button(frame_botones, text="Gestión de Tratamiento", font=("Roboto", 15), bg="#e4c09f", height=2, width=96,command=self.abrir_tratamiento)
         btn_tratamiento.grid(row=5, column=0, pady=(0, 20))    
 
-        btn_ficha = Button(menu, text="Gestión de Ficha Médica", font=("Roboto", 15), bg="#e4c09f", width=96)
-        btn_ficha.grid(row=6, column=0, pady=(0, 20))
+        """btn_ficha = Button(menu, text="Gestión de Ficha Médica", font=("Roboto", 15), bg="#e4c09f", width=96)
+        btn_ficha.grid(row=6, column=0, pady=(0, 20))"""
 
 
 if __name__ == "__main__":
     ventana = Tk()
     ventana.wm_title("Menú Recupero de Obra Social")
     ventana.wm_resizable(0,0)
-    ventana.geometry("+0+0")
     menu = MENU(ventana)
     menu.mainloop()
